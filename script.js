7f0ff28159dca3b896e8bce7d393198c4a1660ac
gsr142 <gsr142@gmail.com>
2023-06-28 19:40:54 -0700

    fix while loop in generatePassword function

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function generatePassword() {
   var pw = "password"
-  var pwLength = prompt("Length (8-128 characters)"); //Gives user a prompt to enter character lenght
+  var pwChars = "abcdefghijklmnopqrstuvwxyz"
+  var upperCase = pwChars.toUpperCase()
+  var numbers = "0123456789"
+  var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+  pwLength = prompt("Length (8-128 characters)"); //Gives user a prompt to enter character length
   isValidInput = false;
 
   //While loop checks if the user entered something and clicked OK
-  while (!isValidInput) {
-    if (pwLength === null) {
-      break;
-    }
+  while (isValidInput === false) {
     //if/else checks that the user entered a number from 8-128, and prompts the user to re-enter the password length if they did not meet these requirements
-    if (!isNaN(parseFloat(pwLength)) && isFinite(pwLength) && pwLength > 7 && pwLength < 129) {
+    if (Number.isInteger(parseFloat(pwLength)) && pwLength > 7 && pwLength < 129) {
       isValidInput = true;
     }
     else {
@@ -21,12 +23,11 @@ function generatePassword() {
       pwLength = prompt("Length (8-128 characters)");
     }
   }
-
-  
   
   var useCaps = confirm("Include Capital Letters?");
   var useNums = confirm("Include Numbers?");
   var useSymbols = confirm("Use Symbols?");
+
   return pw
 }
 function writePassword() {
