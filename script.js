d299518954bcdce553f4175309275073b438d8c2
gsr142 <gsr142@gmail.com>
2023-06-28 20:29:57 -0700

    add user choices for pw character set

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//declare variables
+var pw = "password"
+var pwChars = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = pwChars.toUpperCase();
+var numbers = "0123456789";
+var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
 // Write password to the #password input
 function generatePassword() {
-  var pw = "password"
-  var pwChars = "abcdefghijklmnopqrstuvwxyz"
-  var upperCase = pwChars.toUpperCase()
-  var numbers = "0123456789"
-  var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
-  pwLength = prompt("Length (8-128 characters)"); //Gives user a prompt to enter character length
+  //Gives user a prompt to enter character length
+  var pwLength = prompt("Length (8-128 characters)"); 
   isValidInput = false;
 
   //While loop checks if the user entered something and clicked OK
@@ -24,9 +26,29 @@ function generatePassword() {
     }
   }
   
+  //converts pwLength from a string to an integer
+  pwLength = parseInt(pwLength)
+  
+  //If user confirms use of capital letters in password, caps are added to pwChars
   var useCaps = confirm("Include Capital Letters?");
+  if (useCaps == true) {
+    pwChars = pwChars.concat(upperCase)
+  }
+  console.log(pwChars)
+
+//If user confirms use of numbers in password, numbers are added to pwChars
   var useNums = confirm("Include Numbers?");
+  if (useNums == true) {
+    pwChars = pwChars.concat(numbers)
+  }
+console.log(pwChars)
+
+  //If user confirms use of symbols in password, symbols are added to pwChars
   var useSymbols = confirm("Use Symbols?");
+  if (useSymbols == true) {
+    pwChars = pwChars.concat(symbols)
+  }
+console.log(pwChars)
 
   return pw
 }
